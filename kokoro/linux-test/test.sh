ec42452f43b7487270348f6677fd7da78b7c4980
Paul Thomson <paulthomson@google.com>
2020-02-20 15:13:53 +0000

    Try running SwiftShader

--- kokoro/linux-test/test.sh
+++ kokoro/linux-test/test.sh
@@ -35,6 +35,14 @@ export CC=/usr/bin/gcc-8
 cd $SRC
 BUILD_SHA=${KOKORO_GITHUB_COMMIT:-$KOKORO_GITHUB_PULL_REQUEST_COMMIT}
 
+function bazel {
+  echo $(date): Starting bazel command $@...
+  $BUILD_ROOT/bazel/bin/bazel \
+    --output_base="${TMP}/bazel_out_dbg" \
+    "$@"
+  echo $(date): bazel command completed.
+}
+
 function test {
     echo $(date): Starting test for $@...
     $BUILD_ROOT/bazel/bin/bazel \
@@ -46,6 +54,24 @@ function test {
         $@
     echo $(date): Tests completed.
 }
+
+# Install Vulkan loader and xvfb (X virtual framebuffer).
+sudo apt-get -qy install libvulkan1 xvfb
+
+# Download and extract prebuilt SwiftShader.
+curl -fsSL -o swiftshader.zip https://github.com/google/gfbuild-swiftshader/releases/download/github%2Fgoogle%2Fgfbuild-swiftshader%2F0bbf7ba9f909092f0328b1d519d5f7db1773be57/gfbuild-swiftshader-0bbf7ba9f909092f0328b1d519d5f7db1773be57-Linux_x64_Debug.zip
+unzip -d swiftshader swiftshader.zip
+
+# Use SwiftShader.
+export VK_ICD_FILENAMES="${SRC}/swiftshader/lib/vk_swiftshader_icd.json"
+export VK_LOADER_DEBUG=all
+
+# Build Vulkan sample binary.
+bazel build -c dbg //cmd/vulkan_sample:vulkan_sample
+
+# Run it.
+SAMPLE_PATH="$(bazel run -c dbg --run_under "echo" //cmd/vulkan_sample:vulkan_sample)"
+timeout --preserve-status -s INT -k 10 5 xvfb-run -e xvfb.log -a "${SAMPLE_PATH}"
 
 # Running all the tests in one go leads to an out-of-memory error on Kokoro, hence the division in smaller test sets
 test tests-core
