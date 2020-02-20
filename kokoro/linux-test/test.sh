acdb49a17b0800aeb51a7d9ecc419b4834bb0fa5
Paul Thomson <paulthomson@google.com>
2020-02-20 16:07:52 +0000

    Run app from build.sh

--- kokoro/linux-test/test.sh
+++ kokoro/linux-test/test.sh
@@ -35,13 +35,7 @@ export CC=/usr/bin/gcc-8
 cd $SRC
 BUILD_SHA=${KOKORO_GITHUB_COMMIT:-$KOKORO_GITHUB_PULL_REQUEST_COMMIT}
 
-function bazel {
-  "${BUILD_ROOT}/bazel/bin/bazel" \
-    --output_base="${TMP}/bazel_out_dbg" \
-    "$@"
-}
-
-function bazel-test {
+function test {
     echo $(date): Starting test for $@...
     $BUILD_ROOT/bazel/bin/bazel \
         --output_base="${TMP}/bazel_out" \
@@ -53,41 +47,11 @@ function bazel-test {
     echo $(date): Tests completed.
 }
 
-# Install Vulkan loader and xvfb (X virtual framebuffer).
-sudo apt-get -qy install libvulkan1 xvfb
-
-# Download and extract prebuilt SwiftShader.
-curl -fsSL -o swiftshader.zip https://github.com/google/gfbuild-swiftshader/releases/download/github%2Fgoogle%2Fgfbuild-swiftshader%2F0bbf7ba9f909092f0328b1d519d5f7db1773be57/gfbuild-swiftshader-0bbf7ba9f909092f0328b1d519d5f7db1773be57-Linux_x64_Debug.zip
-unzip -d swiftshader swiftshader.zip
-
-# Use SwiftShader.
-export VK_ICD_FILENAMES="${SRC}/swiftshader/lib/vk_swiftshader_icd.json"
-export VK_LOADER_DEBUG=all
-
-# Build Vulkan sample binary.
-bazel build -c dbg //cmd/vulkan_sample:vulkan_sample
-
-# Run it.
-
-SAMPLE_PATH="$(bazel run -c dbg --run_under "echo" //cmd/vulkan_sample:vulkan_sample)"
-
-# Allow non-zero exit status.
-set +e
-
-timeout --preserve-status -s INT -k 10 5 xvfb-run -e xvfb.log -a "${SAMPLE_PATH}"
-APP_EXIT_STATUS="${?}"
-
-set -e
-
-# This line will exit with status 1 if the app's exit status
-# was anything other than 130 (128+SIGINT).
-test "${APP_EXIT_STATUS}" -eq 130
-
 # Running all the tests in one go leads to an out-of-memory error on Kokoro, hence the division in smaller test sets
-bazel-test tests-core
-bazel-test tests-gapis-api
-bazel-test tests-gapis-replay-resolve
-bazel-test tests-gapis-other
-bazel-test tests-gapir
-bazel-test tests-gapil
-bazel-test tests-general
+test tests-core
+test tests-gapis-api
+test tests-gapis-replay-resolve
+test tests-gapis-other
+test tests-gapir
+test tests-gapil
+test tests-general
--- kokoro/linux/build.sh
+++ kokoro/linux/build.sh
@@ -81,6 +81,42 @@ do
 done
 echo $(date): Smoketests completed.
 
+set -e
+
 # Build the release packages.
 mkdir $BUILD_ROOT/out
 $SRC/kokoro/linux/package.sh $BUILD_ROOT/out
+
+##
+## Test capture and replay of the Vulkan Sample App.
+##
+
+# Install the Vulkan loader and xvfb (X virtual framebuffer).
+sudo apt-get -qy install libvulkan1 xvfb
+
+# Get prebuilt SwiftShader.
+curl -fsSL -o swiftshader.zip https://github.com/google/gfbuild-swiftshader/releases/download/github%2Fgoogle%2Fgfbuild-swiftshader%2F0bbf7ba9f909092f0328b1d519d5f7db1773be57/gfbuild-swiftshader-0bbf7ba9f909092f0328b1d519d5f7db1773be57-Linux_x64_Debug.zip
+unzip -d swiftshader swiftshader.zip
+
+# Use SwiftShader.
+export VK_ICD_FILENAMES="${SRC}/swiftshader/lib/vk_swiftshader_icd.json"
+export VK_LOADER_DEBUG=all
+
+# Just try running the app first.
+
+# Allow non-zero exit status.
+set +e
+
+xvfb-run -e xvfb.log -a timeout --preserve-status -s INT -k 10 5 bazel-bin/cmd/vulkan_sample/vulkan_sample
+
+APP_EXIT_STATUS="${?}"
+
+set -e
+
+if test -f xvfb.log; then
+  cat xvfb.log
+fi
+
+# This line will exit with status 1 if the app's exit status
+# was anything other than 130 (128+SIGINT).
+test "${APP_EXIT_STATUS}" -eq 130
