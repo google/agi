c2ca8125cd403894759cb339afc9a47589d3f516
Stella <stellama@google.com>
2020-02-26 20:37:06 -0800

    Add selection behavior for Memory Panel.
     - Bug: http://b/147922469. #1.

--- gapic/src/main/com/google/gapid/perfetto/models/Selection.java
+++ gapic/src/main/com/google/gapid/perfetto/models/Selection.java
@@ -209,6 +209,7 @@ public interface Selection<Key> {
     public static final Kind<Long> VulkanEvent = new Kind<Long>(4);
     public static final Kind<Long> Counter = new Kind<Long>(5);
     public static final Kind<FrameEventsTrack.Slice.Key> FrameEvents = new Kind<FrameEventsTrack.Slice.Key>(6);
+    public static final Kind<Long> Memory = new Kind<Long>(7);
 
     public int priority;
 
