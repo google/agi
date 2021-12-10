google/gapid

commit a133d59fba1deb64cb15805cde5897b44b0ecbc5

Fix vertex attribute divisor (#950)
- Fix VertexInputData and VertexAttributeDivisor
- Make VertexInputData optional(as spec states)
- Add VertexAttributeDivisor to stateRebuilder
- Fix the code around these two changes
- update the sample trace

gapis/api/vulkan/state_rebuilder.go
@@ -2643,6 +2643,34 @@ func (sb *stateBuilder) createGraphicsPipeline(gp GraphicsPipelineObjectʳ) {
 		))
 	}
 
+	vertexInputState := NewVkPipelineVertexInputStateCreateInfoᶜᵖ(memory.Nullptr)
+	if !gp.VertexInputState().IsNil() {
+		pNext := NewVoidᶜᵖ(memory.Nullptr)
+		if !gp.VertexInputState().DivisorDescriptions().IsNil() {
+			pNext = NewVoidᶜᵖ(sb.MustAllocReadData(
+				NewVkPipelineVertexInputDivisorStateCreateInfoEXT(
+					VkStructureType_VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_DIVISOR_STATE_CREATE_INFO_EXT, // sType
+					pNext, // pNext
+					gp.VertexInputState().DivisorDescriptions().VertexBindingDivisorCount(), // vertexBindingDivisorCount
+					NewVkVertexInputBindingDivisorDescriptionEXTᶜᵖ(sb.MustUnpackReadMap(
+						gp.VertexInputState().DivisorDescriptions().VertexBindingDivisors().All(),
+					).Ptr()), // pVertexBindingDivisors
+				),
+			).Ptr())
+		}
+
+		vertexInputState = NewVkPipelineVertexInputStateCreateInfoᶜᵖ(sb.MustAllocReadData( // pVertexInputState
+			NewVkPipelineVertexInputStateCreateInfo(
+				VkStructureType_VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO, // sType
+				pNext, // pNext
+				0,     // flags
+				uint32(gp.VertexInputState().BindingDescriptions().Len()),                                                               // vertexBindingDescriptionCount
+				NewVkVertexInputBindingDescriptionᶜᵖ(sb.MustUnpackReadMap(gp.VertexInputState().BindingDescriptions().All()).Ptr()),     // pVertexBindingDescriptions
+				uint32(gp.VertexInputState().AttributeDescriptions().Len()),                                                             // vertexAttributeDescriptionCount
+				NewVkVertexInputAttributeDescriptionᶜᵖ(sb.MustUnpackReadMap(gp.VertexInputState().AttributeDescriptions().All()).Ptr()), // pVertexAttributeDescriptions
+			)).Ptr())
+	}
+
 	tessellationState := NewVkPipelineTessellationStateCreateInfoᶜᵖ(memory.Nullptr)
 	if !gp.TessellationState().IsNil() {
 		pNext := NewVoidᶜᵖ(memory.Nullptr)
@@ -2837,16 +2865,7 @@ func (sb *stateBuilder) createGraphicsPipeline(gp GraphicsPipelineObjectʳ) {
 			gp.Flags(),          // flags
 			uint32(len(stages)), // stageCount
 			NewVkPipelineShaderStageCreateInfoᶜᵖ(sb.MustAllocReadData(stages).Ptr()), // pStages
-			NewVkPipelineVertexInputStateCreateInfoᶜᵖ(sb.MustAllocReadData( // pVertexInputState
-				NewVkPipelineVertexInputStateCreateInfo(
-					VkStructureType_VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO, // sType
-					0, // pNext
-					0, // flags
-					uint32(gp.VertexInputState().BindingDescriptions().Len()),                                                               // vertexBindingDescriptionCount
-					NewVkVertexInputBindingDescriptionᶜᵖ(sb.MustUnpackReadMap(gp.VertexInputState().BindingDescriptions().All()).Ptr()),     // pVertexBindingDescriptions
-					uint32(gp.VertexInputState().AttributeDescriptions().Len()),                                                             // vertexAttributeDescriptionCount
-					NewVkVertexInputAttributeDescriptionᶜᵖ(sb.MustUnpackReadMap(gp.VertexInputState().AttributeDescriptions().All()).Ptr()), // pVertexAttributeDescriptions
-				)).Ptr()),
+			vertexInputState, // pVertexInputState
 			NewVkPipelineInputAssemblyStateCreateInfoᶜᵖ(sb.MustAllocReadData( // pInputAssemblyState
 				NewVkPipelineInputAssemblyStateCreateInfo(
 					VkStructureType_VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO, // sType

gapis/api/vulkan/api/coherent_memory.api
@@ -488,44 +488,53 @@ sub void trackMemoryInCurrentPipelineBoundVertexBuffers(u32 vertexCount, u32 ins
   // pseudo-code at the end of section "Vertex Attribute Divisor in Instanced
   // Rendering" in the Vulkan specification.
   ldi := lastDrawInfo()
-  for _ , _ , vertex_binding in ldi.GraphicsPipeline.VertexInputState.BindingDescriptions {
-    if vertex_binding.binding in ldi.BoundVertexBuffers {
-      bound_vertex_buffer := ldi.BoundVertexBuffers[vertex_binding.binding]
-      backing_buf := bound_vertex_buffer.Buffer
-      if backing_buf.Memory != null {
-        start_vertex := switch vertex_binding.inputRate {
-          case VK_VERTEX_INPUT_RATE_VERTEX:
-            firstVertex
-          case VK_VERTEX_INPUT_RATE_INSTANCE:
-            firstInstance
-        }
-        num_vertices := switch vertex_binding.inputRate {
-          case VK_VERTEX_INPUT_RATE_VERTEX:
-            vertexCount
-          case VK_VERTEX_INPUT_RATE_INSTANCE:
-            switch vertex_binding.binding in ldi.GraphicsPipeline.VertexInputState.Divisors {
-              case false:
-                instanceCount
-              case true:
-                switch ldi.GraphicsPipeline.VertexInputState.Divisors[vertex_binding.binding] {
-                  case 0:
-                    as!u32(1) // If divisor is zero, use only one instance
-                  default:
-                    (instanceCount / ldi.GraphicsPipeline.VertexInputState.Divisors[vertex_binding.binding])
-                }
-            }
-        }
-        if num_vertices > 0 {
-          vertex_attrib := ldi.GraphicsPipeline.VertexInputState.AttributeDescriptions[vertex_binding.binding]
-          vertex_attrib_size := getElementAndTexelBlockSize(vertex_attrib.format).ElementSize
-          start_offset := bound_vertex_buffer.Offset + as!VkDeviceSize((start_vertex * vertex_binding.stride) + vertex_attrib.offset)
-          num := switch vertexCount == 0xFFFFFFFF {
-            case true:
-              backing_buf.Info.Size - start_offset
-            case false:
-              as!VkDeviceSize(((num_vertices - 1) * vertex_binding.stride) + vertex_attrib_size)
+  vertexInputState := ldi.GraphicsPipeline.VertexInputState
+  if vertexInputState != null {
+    for _ , _ , vertex_binding in vertexInputState.BindingDescriptions {
+      if vertex_binding.binding in ldi.BoundVertexBuffers {
+        bound_vertex_buffer := ldi.BoundVertexBuffers[vertex_binding.binding]
+        backing_buf := bound_vertex_buffer.Buffer
+        if backing_buf.Memory != null {
+          start_vertex := switch vertex_binding.inputRate {
+            case VK_VERTEX_INPUT_RATE_VERTEX:
+              firstVertex
+            case VK_VERTEX_INPUT_RATE_INSTANCE:
+              firstInstance
           }
-          readMemoryInBuffer(backing_buf, start_offset, num)
+
+          num_vertices := switch vertex_binding.inputRate {
+            case VK_VERTEX_INPUT_RATE_VERTEX:
+              vertexCount
+            case VK_VERTEX_INPUT_RATE_INSTANCE:
+              switch vertexInputState.DivisorDescriptions == null {
+                case false:
+                  instanceCount
+                case true:
+                  switch vertex_binding.binding in vertexInputState.DivisorDescriptions.VertexBindingDivisors {
+                  case false:
+                    instanceCount
+                  case true:
+                    switch vertexInputState.DivisorDescriptions.VertexBindingDivisors[vertex_binding.binding].divisor {
+                      case 0:
+                        as!u32(1) // If divisor is zero, use only one instance
+                      default:
+                        (instanceCount / vertexInputState.DivisorDescriptions.VertexBindingDivisors[vertex_binding.binding].divisor)
+                    }
+                }
+              }
+          }
+          if num_vertices > 0 {
+            vertex_attrib := vertexInputState.AttributeDescriptions[vertex_binding.binding]
+            vertex_attrib_size := getElementAndTexelBlockSize(vertex_attrib.format).ElementSize
+            start_offset := bound_vertex_buffer.Offset + as!VkDeviceSize((start_vertex * vertex_binding.stride) + vertex_attrib.offset)
+            num := switch vertexCount == 0xFFFFFFFF {
+              case true:
+                backing_buf.Info.Size - start_offset
+              case false:
+                as!VkDeviceSize(((num_vertices - 1) * vertex_binding.stride) + vertex_attrib_size)
+            }
+            readMemoryInBuffer(backing_buf, start_offset, num)
+          }
         }
       }
     }

gapis/api/vulkan/api/pipeline.api
@@ -106,10 +106,17 @@ cmd void vkDestroyPipelineLayout(
   @unused ref!SpecializationInfo Specialization
 }
 
-@internal class VertexData {
+@internal class DivisorDescriptionData {
+  @unused u32 VertexBindingDivisorCount
+  @unused map!(u32, VkVertexInputBindingDivisorDescriptionEXT) VertexBindingDivisors
+}
+
+@internal class VertexInputData {
   @unused map!(u32, VkVertexInputBindingDescription)   BindingDescriptions
   @unused map!(u32, VkVertexInputAttributeDescription) AttributeDescriptions
-  @unused map!(u32, u32)                               Divisors
+
+  // @extension(ext_vertex_attribute_divisor)
+  @unused ref!DivisorDescriptionData DivisorDescriptions
 }
 
 @internal class InputAssemblyData {
@@ -201,7 +208,7 @@ cmd void vkDestroyPipelineLayout(
   @unused VkPipelineCreateFlags     Flags
   @unused VkPipeline                VulkanHandle
   @unused map!(u32, StageData)      Stages
-  @unused VertexData                VertexInputState
+  @unused ref!VertexInputData       VertexInputState
   @unused InputAssemblyData         InputAssemblyState
   @unused ref!TessellationStateData TessellationState
   @unused ref!ViewportData          ViewportState
@@ -355,40 +362,47 @@ cmd VkResult vkCreateGraphicsPipelines(
       obj.Stages[j] = stage_data
     }
 
-    vertex_input_state := create_info.pVertexInputState[0]
-    vertex_descriptions := vertex_input_state.pVertexBindingDescriptions[0:
-    vertex_input_state.vertexBindingDescriptionCount]
-    // handle vertex input state pNext
-    if vertex_input_state.pNext != null {
-      numPNext := numberOfPNext(vertex_input_state.pNext)
-      next := MutableVoidPtr(as!void*(vertex_input_state.pNext))
-      for i in (0 .. numPNext) {
-        sType := as!const VkStructureType*(next.Ptr)[0]
-        switch (sType) {
-          case VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_DIVISOR_STATE_CREATE_INFO_EXT: {
-            divisorInfo := as!VkPipelineVertexInputDivisorStateCreateInfoEXT*(next.Ptr)[0]
-            numDivisors := divisorInfo.vertexBindingDivisorCount
-            divisors := divisorInfo.pVertexBindingDivisors[0:numDivisors]
-            for j in (0 .. numDivisors) {
-              d := divisors[j]
-              obj.VertexInputState.Divisors[d.binding] = d.divisor
+    if (create_info.pVertexInputState != null) {
+      vertex_input_state := create_info.pVertexInputState[0]
+      obj.VertexInputState = new!VertexInputData()
+
+      // handle vertex input state pNext
+      if vertex_input_state.pNext != null {
+        numPNext := numberOfPNext(vertex_input_state.pNext)
+        next := MutableVoidPtr(as!void*(vertex_input_state.pNext))
+        for i in (0 .. numPNext) {
+          sType := as!const VkStructureType*(next.Ptr)[0]
+          switch (sType) {
+            case VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_DIVISOR_STATE_CREATE_INFO_EXT: {
+              ext := as!VkPipelineVertexInputDivisorStateCreateInfoEXT*(next.Ptr)[0]
+              numDivisors := ext.vertexBindingDivisorCount
+              divisors := ext.pVertexBindingDivisors[0:numDivisors]
+              obj.VertexInputState.DivisorDescriptions = new!DivisorDescriptionData(
+                VertexBindingDivisorCount : numDivisors,
+              )
+              for j in (0 .. numDivisors) {
+                obj.VertexInputState.DivisorDescriptions.VertexBindingDivisors[divisors[j].binding] = divisors[j]
+              }
             }
           }
+          next.Ptr = as!VulkanStructHeader*(next.Ptr)[0].PNext
         }
-        next.Ptr = as!VulkanStructHeader*(next.Ptr)[0].PNext
       }
-    }
-    for b in (0 .. vertex_input_state.vertexBindingDescriptionCount) {
-      vertex_binding := vertex_descriptions[b]
-      obj.VertexInputState.BindingDescriptions[b] = vertex_binding
-    }
 
-    vertex_attributes := vertex_input_state.pVertexAttributeDescriptions[0:
-    vertex_input_state.vertexAttributeDescriptionCount]
+      vertex_descriptions := vertex_input_state.pVertexBindingDescriptions[0:
+        vertex_input_state.vertexBindingDescriptionCount]
+      for b in (0 .. vertex_input_state.vertexBindingDescriptionCount) {
+        vertex_binding := vertex_descriptions[b]
+        obj.VertexInputState.BindingDescriptions[b] = vertex_binding
+      }
 
-    for b in (0 .. vertex_input_state.vertexAttributeDescriptionCount) {
-      attribute := vertex_attributes[b]
-      obj.VertexInputState.AttributeDescriptions[b] = attribute
+      vertex_attributes := vertex_input_state.pVertexAttributeDescriptions[0:
+      vertex_input_state.vertexAttributeDescriptionCount]
+
+      for b in (0 .. vertex_input_state.vertexAttributeDescriptionCount) {
+        attribute := vertex_attributes[b]
+        obj.VertexInputState.AttributeDescriptions[b] = attribute
+      }
     }
 
     input_assembly_state := create_info.pInputAssemblyState[0]

google/agi

commit 7bd5a72c0dcda22b6f4e0fa518a746c3cca0a016

Fix a regression introduced by #950.
The map of divisors is not a dense map. The existing map unpacking
functions require dense maps as documented. This change introduces a new
function that will properly unpack a non-dense map, ignoring the keys.

gapis/api/vulkan/command_buffer_rebuilder.go
@@ -45,11 +45,38 @@ func unpackDenseMapWithAllocator(alloc func(v ...interface{}) api.AllocResult, m
 	return alloc(sl.Interface()), uint32(d.Len())
 }
 
+// unpackMapWithAllocator takes a map of any key -> structure, flattens the
+// map into a slice, allocates the appropriate data using a custom provided
+// allocation function and returns it as well as the length of the map. This is
+// similar to unpackMapWithAllocator, except that the keys are ignored.
+func unpackMapWithAllocator(alloc func(v ...interface{}) api.AllocResult, m interface{}) (api.AllocResult, uint32) {
+	d := dictionary.From(m)
+	sl := reflect.MakeSlice(reflect.SliceOf(d.ValTy()), d.Len(), d.Len())
+	i := 0
+	for _, e := range dictionary.Entries(d) {
+		v := reflect.ValueOf(e.V)
+		sl.Index(i).Set(v)
+		i++
+	}
+
+	return alloc(sl.Interface()), uint32(d.Len())
+}
+
 // unpackDenseMap takes a dense map of u32 -> structure, flattens the map into
 // a slice, allocates the appropriate data and returns it as well as the
 // length of the map.
 func unpackDenseMap(ctx context.Context, s *api.GlobalState, m interface{}) (api.AllocResult, uint32) {
 	return unpackDenseMapWithAllocator(func(v ...interface{}) api.AllocResult {
+		return s.AllocDataOrPanic(ctx, v...)
+	}, m)
+}
+
+// unpackMap takes a map of any key -> structure, flattens the map into
+// a slice, allocates the appropriate data and returns it as well as the
+// length of the map. This is similar to unpackMap, except that the keys are
+// ignored.
+func unpackMap(ctx context.Context, s *api.GlobalState, m interface{}) (api.AllocResult, uint32) {
+	return unpackMapWithAllocator(func(v ...interface{}) api.AllocResult {
 		return s.AllocDataOrPanic(ctx, v...)
 	}, m)
 }

gapis/api/vulkan/state_rebuilder.go
@@ -364,6 +364,14 @@ func (sb *stateBuilder) MustUnpackReadDenseMap(v interface{}) api.AllocResult {
 	return allocateResult
 }
 
+func (sb *stateBuilder) MustUnpackReadMap(v interface{}) api.AllocResult {
+	allocateResult, _ := unpackMap(sb.ctx, sb.newState, v)
+	sb.readMemories = append(sb.readMemories, &allocateResult)
+	rng := allocateResult.Range()
+	interval.Merge(&sb.memoryIntervals, interval.U64Span{rng.Base, rng.Base + rng.Size}, true)
+	return allocateResult
+}
+
 func (sb *stateBuilder) MustUnpackWriteDenseMap(v interface{}) api.AllocResult {
 	allocateResult, _ := unpackDenseMap(sb.ctx, sb.newState, v)
 	sb.writeMemories = append(sb.writeMemories, &allocateResult)
@@ -2698,7 +2706,7 @@ func (sb *stateBuilder) createGraphicsPipeline(gp GraphicsPipelineObjectʳ) {
 					VkStructureType_VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_DIVISOR_STATE_CREATE_INFO_EXT, // sType
 					pNext, // pNext
 					gp.VertexInputState().DivisorDescriptions().VertexBindingDivisorCount(), // vertexBindingDivisorCount
-					NewVkVertexInputBindingDivisorDescriptionEXTᶜᵖ(sb.MustUnpackReadDenseMap(
+					NewVkVertexInputBindingDivisorDescriptionEXTᶜᵖ(sb.MustUnpackReadMap(
 						gp.VertexInputState().DivisorDescriptions().VertexBindingDivisors().All(),
 					).Ptr()), // pVertexBindingDivisors
 				),