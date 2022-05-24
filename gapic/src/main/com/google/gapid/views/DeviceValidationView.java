026dad24387dec19371633892ab4fbbaf82e9707
Tanat Boozayaangool <tanatb@google.com>
2022-05-24 17:12:18 +0100

    Add Validation UI to handle different error codes

--- gapic/src/main/com/google/gapid/views/DeviceValidationView.java
+++ gapic/src/main/com/google/gapid/views/DeviceValidationView.java
@@ -17,17 +17,23 @@ package com.google.gapid.views;
 
 import static com.google.gapid.util.Logging.throttleLogRpcError;
 import static com.google.gapid.widgets.Widgets.createLink;
+import static com.google.gapid.widgets.Widgets.createBoldLabel;
 import static com.google.gapid.widgets.Widgets.withLayoutData;
+import static com.google.gapid.widgets.Widgets.withMargin;
+import static com.google.gapid.widgets.Widgets.withMarginAndSpacing;
+import static com.google.gapid.widgets.Widgets.withSpacing;
 import static com.google.gapid.widgets.Widgets.withSpans;
 import static com.google.gapid.widgets.Widgets.createButton;
 import static com.google.gapid.widgets.Widgets.createComposite;
 import static com.google.gapid.widgets.Widgets.createGroup;
 import static com.google.gapid.widgets.Widgets.createLabel;
 import static com.google.gapid.widgets.Widgets.createTextbox;
+import static com.google.gapid.widgets.Widgets.removeAllSelectionListeners;
 import static java.util.logging.Level.WARNING;
 import static java.util.logging.Level.SEVERE;
 
 import com.google.gapid.models.Devices.DeviceValidationResult;
+import com.google.gapid.models.Devices.DeviceValidationResult.ErrorCode;
 import com.google.gapid.models.Devices.DeviceCaptureInfo;
 import com.google.gapid.models.Models;
 import com.google.gapid.proto.device.Device;
@@ -38,6 +44,7 @@ import com.google.gapid.rpc.SingleInFlight;
 import com.google.gapid.rpc.UiErrorCallback;
 import com.google.gapid.widgets.LoadingIndicator;
 import com.google.gapid.widgets.Widgets;
+import com.google.gapid.util.Logging;
 import com.google.gapid.util.Messages;
 import com.google.gapid.util.OS;
 import com.google.gapid.util.URLs;
@@ -64,6 +71,7 @@ import java.util.logging.Logger;
 
 public class DeviceValidationView extends Composite {
   protected static final Logger LOG = Logger.getLogger(DeviceValidationView.class.getName());
+  private static final int TRACE_VALIDATION_FAILURE_COUNT = 0;
 
   private final Widgets widgets;
   private final Models models;
@@ -71,12 +79,14 @@ public class DeviceValidationView extends Composite {
     
   private boolean validationPassed;
   private LoadingIndicator.Widget statusLoader;
-  private Label statusText;
+  private Link statusText;
   private Button retryButton;
 
   // Separate error text from help textas a workaround to enable proper text wrapping.
   private Group errorMessageGroup;
   private Composite helpComposite;
+
+  private int traceValidationFailureCount;
 
   public DeviceValidationView(Composite parent, Models models, Widgets widgets) {
     super(parent, SWT.NONE);
@@ -92,8 +102,9 @@ public class DeviceValidationView extends Composite {
     statusLoader = widgets.loading.createWidgetWithImage(this, 
         widgets.theme.check(), widgets.theme.error());
     statusLoader.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-    statusText = withLayoutData(createLabel(this, ""),
-      new GridData(SWT.FILL, SWT.CENTER, true, false));
+    statusText = withLayoutData(createLink(this, "", e -> {
+      // Intentionally empty.
+    }), new GridData(SWT.FILL, SWT.CENTER, true, false));
     retryButton = withLayoutData(createButton(this, "Retry", e -> {
       // Intentionally empty.
     }), new GridData(SWT.FILL, SWT.CENTER, false, true));
@@ -101,6 +112,8 @@ public class DeviceValidationView extends Composite {
     statusLoader.setVisible(false);
     statusText.setVisible(false);
     retryButton.setVisible(false);
+
+    traceValidationFailureCount = 0;
   }
 
   public void ValidateDevice(DeviceCaptureInfo deviceInfo) {
@@ -128,7 +141,7 @@ public class DeviceValidationView extends Composite {
 
     DeviceValidationResult cachedResult = models.devices.getCachedValidationStatus(dev);
     if (cachedResult != null) {
-      if (cachedResult.passed || cachedResult.skipped) {
+      if (cachedResult.passedOrSkipped()) {
         setValidationStatus(cachedResult);
         return;
       }
@@ -141,6 +154,7 @@ public class DeviceValidationView extends Composite {
     for (Listener listener : retryButton.getListeners(SWT.Selection)) {
       retryButton.removeListener(SWT.Selection, listener);
     }
+    removeAllSelectionListeners(retryButton);
     retryButton.addListener(SWT.Selection, e -> {
       ValidateDevice(dev);
     });
@@ -149,41 +163,114 @@ public class DeviceValidationView extends Composite {
   }
 
   private void setValidationStatus(DeviceValidationResult result) {
-    boolean passedOrSkipped = result.passed || result.skipped;
+    boolean passedOrSkipped = result.passedOrSkipped();
     statusLoader.stopLoading();
     statusLoader.updateStatus(passedOrSkipped);
     validationPassed = passedOrSkipped;
-    statusText.setText("Device support validation " + result.toString() + ".");
+    statusText.setText(getResultMessage(result.errorCode));
+    removeAllSelectionListeners(statusText);
+    if (result.errorCode == ErrorCode.FailedPrecondition) {
+      statusText.addListener(SWT.Selection, (e) -> {
+        Program.launch(URLs.DEVICE_COMPATIBILITY_URL);
+      });
+    }
     notifyListeners(SWT.Modify, new Event());
 
     if (passedOrSkipped) {
       return;
     }
 
-    // Extra details (i.e. error message & help text)
-    retryButton.setVisible(true);
+    // Only show the retry button for non-deterministic errors.    
+    if (result.errorCode == ErrorCode.FailedTraceValidation) {
+      retryButton.setVisible(true);
+      traceValidationFailureCount++;
+    }
+
+    helpComposite = createComposite(this, 
+    withMarginAndSpacing(new GridLayout(1, false), 0, 0, 0, 0));
+    helpComposite.setLayoutData(withSpans(new GridData(), 3, 1));
+    addExplanationMessage(helpComposite, result.errorCode);
+
+    // Only show the file bug section if we encounter an unexpected error.
+    if (result.errorCode == ErrorCode.Internal || result.errorCode == ErrorCode.Invalid)
+      addFileBugSection(helpComposite, result.tracePath, /* addNotice= */ false);
+    else if (result.errorCode == ErrorCode.FailedTraceValidation 
+      && traceValidationFailureCount >= TRACE_VALIDATION_FAILURE_COUNT) {
+      addFileBugSection(helpComposite, result.tracePath, /* addNotice= */ true);
+    }
 
     errorMessageGroup = withLayoutData(createGroup(this, ""),
       withSpans(new GridData(SWT.FILL, SWT.TOP, true, false), 3, 1));
     Text errText = withLayoutData(
-      createTextbox(errorMessageGroup, SWT.READ_ONLY | SWT.WRAP , result.validationFailureMsg),
+      createTextbox(errorMessageGroup, SWT.READ_ONLY | SWT.WRAP,  result.toString()),
       new GridData(SWT.LEFT, SWT.TOP, false, false));
 
-    helpComposite = createComposite(this, new GridLayout(1, false));
-    helpComposite.setLayoutData(withSpans(new GridData(), 3, 1));
-    Link helpLink = withLayoutData(createLink(helpComposite, Messages.VALIDATION_FAILED_LANDING_PAGE, e -> {
-      Program.launch(URLs.DEVICE_COMPATIBILITY_URL);
-    }), new GridData(SWT.LEFT, SWT.TOP, false, false));
-    if (result.tracePath.length() > 0) {
-      Link traceLink = withLayoutData(createLink(helpComposite, "View <a>trace file</a>", e -> {
-        try {
-          OS.openFileInSystemExplorer(new File(result.tracePath));
-        } catch (IOException exception) {
-          LOG.log(SEVERE, "Failed to open log directory in system explorer", exception);
-        }
-      }), new GridData(SWT.LEFT, SWT.TOP, false, false));
+    resizeWindow();
+  }
+
+  private String getResultMessage(DeviceValidationResult.ErrorCode errorCode) {
+    switch(errorCode) {
+      case Ok:
+        return "Device successfully validated.";
+      case FailedPrecondition:
+        return "This device is not supported, learn more <a>here</a>.";
+      case FailedTraceValidation:
+        return "Failed to validate the trace of the sample application.";
+      default:
+        return "Encountered an unexpected error during validation.";
+    }
+  }
+
+  private void addExplanationMessage(Composite parent, DeviceValidationResult.ErrorCode errorCode) {
+    switch(errorCode) {
+      case FailedPrecondition:
+        createLabel(parent,
+         "This may be caused by using an older device or an unsupported operating system.");
+        break;
+      case FailedTraceValidation:
+        createLabel(parent, "The validation process can be non deterministic. Please retry while making sure that:");
+        createLabel(parent, "\t- The screen remains unlocked");
+        createLabel(parent, "\t- The sample application remains in focus");
+        break;
+      default:
+        return;
+    }
+  }
+
+  private void addFileBugSection(Composite parent, String tracePath, boolean addNotice) {
+    Composite composite = createComposite(parent, 
+      withMarginAndSpacing(new GridLayout(1, false), 0, 5, 0, 0));
+    composite.setLayoutData(withSpans(new GridData(GridData.VERTICAL_ALIGN_END), 3, 1));
+
+    if (addNotice) {
+      withLayoutData(createBoldLabel(composite, "If this continues to persist,"),
+        new GridData(GridData.VERTICAL_ALIGN_END));
     }
 
+    Link bugLink = withLayoutData(createLink(composite, "please file a <a>bug report</a> with:", e -> {
+      Program.launch(URLs.FILE_BUG_URL);
+    }), new GridData(GridData.VERTICAL_ALIGN_END));
+    Link serverLog = createLink(composite, "\t - The <a>server logs</a>",
+      openFileAt(Logging.getServerLogFile()));
+    if (tracePath.length() > 0) {
+      Link traceFile = createLink(composite, "\t - The <a>trace file</a>", 
+        openFileAt(new File(tracePath)));
+    }
+    Link clientLog = createLink(composite, "\t - The <a>client logs</a>",
+      openFileAt(Logging.getClientLogFile()));
+  }
+
+  private Listener openFileAt(File file) {
+    return e -> {
+      try {
+        OS.openFileInSystemExplorer(file);
+      } catch (IOException exception) {
+        LOG.log(SEVERE, "Failed to open log directory in system explorer", exception);
+      }
+    };
+  }
+
+  private void resizeWindow() {
     // Resize the rest of the window if needed.
     Point curr = getShell().getSize();
     Point want = getShell().computeSize(curr.x, SWT.DEFAULT);
