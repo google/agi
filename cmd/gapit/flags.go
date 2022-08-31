ddf4b16a76b9faf9af013f85e4323ca061fb8979
Alan <purvisa@google.com>
2022-08-31 16:14:21 +0100

    fix presubmit

--- cmd/gapit/flags.go
+++ cmd/gapit/flags.go
@@ -323,7 +323,7 @@ type (
 			Frames int `help:"only capture the given number of frames. 0 for all"`
 		}
 		UseAndroidFrameBoundary bool `help:"When enabled, AGI uses ANDROID_frame_boundary extension as frame delimiters. Otherwise, present calls are used. Default is false."`
-		No                         struct {
+		No                      struct {
 			Buffer bool `help:"Do not buffer the output, this helps if the application crashes"`
 		}
 		API   string `help:"only capture the given API valid options are vulkan, angle and perfetto"`
