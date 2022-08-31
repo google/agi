6b64552bd743b3b7e012e0e7108e7f89ce4f1d51
Alan <purvisa@google.com>
2022-08-31 12:36:12 +0100

    Invert gapit frame boundary flag for sensible default value

--- cmd/gapit/flags.go
+++ cmd/gapit/flags.go
@@ -322,7 +322,7 @@ type (
 		Capture struct {
 			Frames int `help:"only capture the given number of frames. 0 for all"`
 		}
-		IgnoreAndroidFrameBoundary bool `help:"If true: ignore the ANDROID_frame_boundary extension. If false: respect it."`
+		UseAndroidFrameBoundary bool `help:"If false: ignore the ANDROID_frame_boundary extension. If true: respect it."`
 		No                         struct {
 			Buffer bool `help:"Do not buffer the output, this helps if the application crashes"`
 		}
