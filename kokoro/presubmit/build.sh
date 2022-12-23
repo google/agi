ed0f52728c92bc5eb897f81c98e9ccf7be0943ea
Melih Yalcin <melihyalcin@google.com>
2022-12-23 18:05:00 +0000

    fix the git error

--- kokoro/presubmit/build.sh
+++ kokoro/presubmit/build.sh
@@ -42,7 +42,7 @@ sudo apt-get -y update
 sudo apt-get install -y clang-format-11
 
 # Get recent Android build tools.
-echo y | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install 'build-tools;30.0.3'
+echo y | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install 'build-tools;30.0.3' 'platforms;android-26'
 
 # Python Format tool
 pip install --root-user-action=ignore --user autopep8==1.6.0
