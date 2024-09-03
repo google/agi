c4ec35893a51bdb4bc6000a2ac383717dfbfccef
Oleh Kuznetsov <okuznetsov@google.com>
2024-09-03 11:48:25 +0100

    Fix loop for build.bat

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -123,22 +123,20 @@ mkdir %BAZEL_OUTPUT_USER_ROOT%
 REM Build in several steps in order to avoid running out of memory.
 
 set BAZEL_JAVAC_OPTS="-J-Xmx4g -J-Xms2g"
-set BUILD_TARGETS=( \
-    "//core/vulkan/vk_virtual_swapchain/apk:VkLayer_VirtualSwapchain" \
-    "@com_github_golang_protobuf//proto:go_default_library @com_github_pkg_errors//:go_default_library" \
-    "//gapis/replay/builder:go_default_library //gapis/replay/value:go_default_library" \
-    "//core/app/status:go_default_library //core/context/keys:go_default_library //core/data:go_default_library",
-    "//core/data/binary:go_default_library //core/data/dictionary:go_default_library //core/data/endian:go_default_library",
-    "//core/data/id:go_default_library //core/data/protoconv:go_default_library //core/event/task:go_default_library",
-    "//core/image:go_default_library //core/image/astc:go_default_library //core/image/etc:go_default_library",
-    "//core/log:go_default_library //core/math/interval:go_default_library //core/math/u64:go_default_library",
-    "//core/os/device:go_default_library //core/stream:go_default_library //core/stream/fmts:go_default_library",
-    "//core/vulkan/loader:go_default_library",
-    "//gapis/api/vulkan:go_default_library" \
-    "//cmd/vulkan_sample:vulkan_sample //tools/logo:agi_ico" \
-    "//:pkg-lib" \
-    "//:pkg" \
-)
+set BUILD_TARGETS=//core/vulkan/vk_virtual_swapchain/apk:VkLayer_VirtualSwapchain
+set BUILD_TARGETS=%BUILD_TARGETS%;@com_github_golang_protobuf//proto:go_default_library @com_github_pkg_errors//:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//gapis/replay/builder:go_default_library //gapis/replay/value:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/app/status:go_default_library //core/context/keys:go_default_library //core/data:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/data/binary:go_default_library //core/data/dictionary:go_default_library //core/data/endian:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/data/id:go_default_library //core/data/protoconv:go_default_library //core/event/task:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/image:go_default_library //core/image/astc:go_default_library //core/image/etc:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/log:go_default_library //core/math/interval:go_default_library //core/math/u64:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/os/device:go_default_library //core/stream:go_default_library //core/stream/fmts:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//core/vulkan/loader:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//gapis/api/vulkan:go_default_library
+set BUILD_TARGETS=%BUILD_TARGETS%;//cmd/vulkan_sample:vulkan_sample //tools/logo:agi_ico
+set BUILD_TARGETS=%BUILD_TARGETS%;//:pkg-lib
+set BUILD_TARGETS=%BUILD_TARGETS%;//:pkg
 
 REM Loop through the build targets
 for %%T in (%BUILD_TARGETS%) do (
